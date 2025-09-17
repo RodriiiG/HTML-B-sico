011f9210de52f62878e34b9c906c4060360d3365
RodriiiG <rodrigo.godoy2712@gmail.com>
2025-09-17 15:55:21 -0300

    bolso terminado

--- index.js
+++ index.js
@@ -8,3 +8,4 @@ function jugar(eleccion) {
   }
   alert(mensaje);
 }
+
---
+++ nacional.js
@@ -0,0 +1,4 @@
+function saludar() {
+  var nombre = document.getElementById("nombre").value;
+  alert("Hola " + nombre + ", gracias por hacerte socio del Club Nacional de Football.");
+}
